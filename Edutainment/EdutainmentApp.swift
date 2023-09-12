5ba5b4ab8d259fdc90788e75ce8132a7f1319522
shomil03 <shomil02020@gmail.com>
2023-09-12 18:58:44 +0530

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Shomil Singh on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
